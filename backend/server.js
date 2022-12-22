6f20160f7d7aa7dd329033d2efca3618c077d9fe
Spoorthi-H <spoorthihm51@gmail.com>
2022-12-22 07:25:12 +0530

    correction2

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ app.use(express.urlencoded({extended:false}));
 app.use("/api/tasks",taskRoutes);
 
 //cors
-/*const corsOptions ={
+const corsOptions ={
     origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));*/
+app.use(cors(corsOptions));
 //Routes
 app.get("/",(req,res)=>{
     res.send("Home Page");
