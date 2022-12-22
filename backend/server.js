14d8b3eac644035e307640c906c7ddc0fae07949
Spoorthi-H <spoorthihm51@gmail.com>
2022-12-22 07:49:35 +0530

    correction2

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,25 @@ app.use(express.urlencoded({extended:false}));
 
 
 //cors
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mern-task-app.onrender.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 const corsOptions ={
     origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"], 
     credentials:true,            //access-control-allow-credentials:true
